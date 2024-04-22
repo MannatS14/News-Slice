e9fd4161a4b487473ca69a084179c0449b997557
Mannat Singh <mannatwadhwa14@gmail.com>
2024-04-23 01:45:28 +0530

    Added Basic UI

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:news_slice/view/widgets/NewsContainer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return NewsContainer(
+                imgUrl:
+                    'https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg',
+                newsHead: 'Meta unveils Llama 3 and real-time image generator',
+                newsDes:
+                    'Meta Platforms, formerly known as Facebook, has taken a significant stride in the field of generative AI with the release of its latest large language model, Llama 3, and a real-time image generator. The move comes as the tech giant aims to close the gap with market leader OpenAI in the rapidly evolving field of artificial intelligence.',
+                newsUrl:
+                    'https://www.businesstoday.in/technology/news/story/meta-unveils-llama-3-and-real-time-image-generator-see-details-426037-2024-04-19');
+          }),
+    );
   }
 }
---
+++ lib/view/widgets/NewsContainer.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class NewsContainer extends StatelessWidget {
+  String imgUrl;
+  String newsHead;
+  String newsDes;
+  String newsUrl;
+  NewsContainer(
+      {super.key,
+      required this.imgUrl,
+      required this.newsDes,
+      required this.newsHead,
+      required this.newsUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+              height: 400,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+              imgUrl),
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  newsHead,
+                  style: const TextStyle(
+                      fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  newsDes,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print("GOING TO $newsUrl");
+                },
+                child: const Text("Read More"),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
